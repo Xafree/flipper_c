c895795c8688fbb84830783e1a890a1355b3aa96
Xafree <emmanueleberst@hotmail.com>
2022-04-25 11:07:17 +0100

    structure d'affichage

--- main_thread.c
+++ main_thread.c
@@ -3,37 +3,52 @@
 #include <pthread.h>
 #include <semaphore.h>
 
+void *routine_monnayeur( void * arg){
+
+    printf("thread crée monnayeur\n");
+
+    pthread_exit(NULL);
+}
+
+
+void *routine_client(){
+    printf("thread crée client\n");
+
+    pthread_exit(NULL);
+}
+
 void *routine_flipper(void *arg) {
     pthread_t t_monnayeur;
     pthread_t t_client;
     sem_t semaphore;
 
-	pthread_create(& t_monnayeur, NULL,routine_monnayeur, NULL );
+    printf("nous somme dans le thread \n");
+
+	pthread_create(& t_monnayeur, NULL, &routine_monnayeur, NULL );
+    pthread_join(t_monnayeur, NULL);
 
 
-	pthread_exit(EXIT_SUCCESS);
+
+    pthread_create(& t_client, NULL, &routine_client, NULL );
+    pthread_join(t_monnayeur, NULL);
+
+	pthread_exit(NULL);
+    free(&t_monnayeur);
 }
 
-void *routine_monnayeur( void * arg){
 
-
-
-    pthread_exit(EXIT_SUCCESS);
-}
-
-
-void *routine_client(){
-
-
-    pthread_exit(EXIT_SUCCESS);
-}
 
 int main(void) {
 	// Création de la variable qui va contenir le thread
 	pthread_t flipper;
 	printf("Avant la création du thread.\n");
+
 	// Création du thread
-	pthread_create(&flipper, NULL, routine_flipper, NULL);
+	pthread_create(&flipper, NULL, &routine_flipper, NULL);
+    pthread_join(flipper, NULL);
+
 	printf("Après la création du thread.\n");
+
 	return EXIT_SUCCESS;
+    free(&flipper);
 }
