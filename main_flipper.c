4301165bd82b07b9c3aeb0b59f3232bb16d56d12
Xafree <emmanueleberst@hotmail.com>
2022-04-27 20:01:48 +0100

    succes return value in parent thread

--- main_flipper.c
+++ main_flipper.c
@@ -6,16 +6,30 @@
 
 
 sem_t semaphore;
-#define MAX_LENGHT 10;
+
+typedef struct thread_data
+{
+    int output;
+} thread_data;
+
+int get_random (int max){
+   double val;
+ 
+   val = (double) max * rand ();
+   val = val / (RAND_MAX + 1.0);
+ 
+   return (int) val;
+}
 
 
 void *routine_partie( void * arg){
     sem_wait(&semaphore);
     printf("thread crée partie\n");
-    fflush(stdout);
+    thread_data *tdata = (thread_data *) arg;
+    tdata->output = get_random(1000000);
     sleep(1);
-    printf("Score du joueur\n");
-    fflush(stdout);
+    printf("Score du joueur : %d\n", tdata->output);
+    
     sem_post(&semaphore);
     pthread_exit(NULL);
 }
@@ -23,15 +37,18 @@ void *routine_partie( void * arg){
 void *routine_monnayeur( void * arg){
     
     int nb_partie = *(int*) arg;
-    pthread_t t_parties[10];
-    printf("NB PARTIE %d",nb_partie);
+    //int *output = malloc(nb_partie * sizeof(int));
+    thread_data *tdata;
+    //int tab_size = malloc(nb_partie * sizeof(int));
+    pthread_t *t_parties = malloc(nb_partie * sizeof(pthread_t));
     printf("thread crée monnayeur\n");
     for(int i = 0; i< nb_partie; i++){
         printf("parti %d \n",i);
-        pthread_create(&t_parties[i], NULL, &routine_partie, NULL);
+        pthread_create(&t_parties[i], NULL, &routine_partie, (void *)&tdata);
         pthread_join(t_parties[i], NULL);
     }
-
+     printf("VALEUR DE OUTPUT : %d \n", &tdata->output);
+    free(t_parties);
     pthread_exit(NULL);
 }
 
@@ -48,7 +65,6 @@ void *routine_flipper(void *arg) {
     pthread_t t_client;
 
     int nb_partie = *(int *)arg;
-    printf("NB PARTIE %d",nb_partie);
 
     printf("nous somme dans le thread \n");
 
@@ -69,15 +85,31 @@ int main(void) {
 	// Création de la variable qui va contenir le thread
 	pthread_t flipper;
     sem_init(&semaphore,0,1);
-    int nb_partie = 5;
-	printf("Avant la création du thread.\n");
+    int nb_partie = 1;
 
-	// Création du thread
-	pthread_create(&flipper, NULL, &routine_flipper, &nb_partie);
-    pthread_join(flipper, NULL);
+    while(nb_partie != 0){
+        printf( "Combien de pièce avez vous-saisie ? \n" );
+        scanf( "%d", &nb_partie );
 
-	printf("Après la création du thread.\n");
+        if( nb_partie >= 1000 || nb_partie == 0){
+            if(nb_partie == 0){
+                printf("Aurevoir !\n");
+            }else{
+                printf("Le nombre de partie maximal est de 10 !\n");
+            }
+            
+        }else{
+            printf("Avant la création du thread.\n");
 
-	return EXIT_SUCCESS;
+            // Création du thread
+            pthread_create(&flipper, NULL, &routine_flipper, &nb_partie);
+            pthread_join(flipper, NULL);
+
+            printf("Après la création du thread.\n");
+        }   
+        
+    }
+    
+    return EXIT_SUCCESS;
     free(&flipper);
 }
